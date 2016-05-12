582d67fa774876878b9151202bc845a28072d419
Greg Johnston <greg.a.johnston@gmail.com>
2016-05-11 22:09:03 -0400

    Created zip code reader

--- weather.py
+++ weather.py
@@ -1,4 +1,19 @@
+import re
+
+
 class Weather:
+
+    @staticmethod
+    def is_zip(zip_code):
+        return (type(zip_code) is str and
+                re.match(r'^[0-9]{5}$', zip_code))
+
+    def welcome_user():
+        print('Welcome to the Weather Forecaster!')
+        print('Please enter your zip code:')
+        zip_code = input('>')
+        if Weather.is_zip(zip_code):
+            return zip_code
 
     def main():
         pass
--- weather_test.py
+++ weather_test.py
@@ -2,9 +2,22 @@ import unittest
 import requests_mock
 
 from current_conditions import CurrentConditions
+from weather import Weather
 
 
 class TestStringMethods(unittest.TestCase):
+
+    def test_is_zip(self):
+        self.assertTrue(Weather.is_zip('12345'))
+
+    def test_is_not_zip_long(self):
+        self.assertFalse(Weather.is_zip('012345'))
+
+    def test_is_not_zip_alpha(self):
+        self.assertFalse(Weather.is_zip('012c45'))
+
+    def test_is_not_zip_numeric(self):
+        self.assertFalse(Weather.is_zip(12345))
 
     @requests_mock.mock()
     def test_web_query(self, mock):
